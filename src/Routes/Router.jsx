b2ef2bc7ead21a6a1b1f524bbd17fd69baaa28dc
Methdec <polowador@gmail.com>
2025-01-24 09:49:13 +0100

    HI3

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router";
 
-import FetchGames from "../components/FetchGames/FetchGames";
+import FetchGames from "../Components/FetchGames/FetchGames";
 import FetchBoss from "../Components/FetchBoss/FetchBoss";
-import FetchMonsters from "../components/FetchMonsters/FetchMonsters";
-import FetchCharacters from "../components/FetchCharacters/FetchCharacters";
-import FetchDungeons from "../components/FetchDungeons/FetchDungeons";
-import Connect from "../components/Connect/Connect";
+import FetchMonsters from "../Components/FetchMonsters/FetchMonsters";
+import FetchCharacters from "../Components/FetchCharacters/FetchCharacters";
+import FetchDungeons from "../Components/FetchDungeons/FetchDungeons";
+import Connect from "../Components/Connect/Connect";
 import Home from "../pages/Home";   
 
 export default function Router() {
